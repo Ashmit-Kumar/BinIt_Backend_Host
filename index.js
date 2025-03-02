78561dab75b77f797f7d6bf475454a8df51578f5
Ashmit <ashmitkumar1020@gmail.com>
2025-03-03 00:49:21 +0530

    Add Cors

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const connectDB  =require('./db/connectDB')
 const reportRoutes=require('./routes/reportRoutes')
 const ngoRegistrationRoutes = require('./routes/ngoRegistrationRoutes');
 const subscribeRoutes = require('./routes/subscribeRoutes');
+const cors = require('cors');
 
 const dbName=process.env.DBNAME;
 const port=process.env.PORT || 8080;
-
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:5173', // Allow only requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  };
+  
 connectDB(dbName);
 const app=express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use('/reports', reportRoutes);
 app.use('/ngo',ngoRegistrationRoutes);
